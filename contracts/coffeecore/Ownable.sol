c8760230a67d97013a9aceae867cde91030d40f2
Cipher2472 <ayushtiwari5747462@gmail.com>
2020-06-25 20:44:01 +0530

    Fixed all bugs in contracts

--- contracts/coffeecore/Ownable.sol
+++ contracts/coffeecore/Ownable.sol
@@ -20,7 +20,7 @@ contract Ownable {
     }
 
     /// Define a function modifier 'onlyOwner'
-    modifier onlyOwner() {
+    modifier  onlyOwner() virtual{
         require(isOwner());
         _;
     }
